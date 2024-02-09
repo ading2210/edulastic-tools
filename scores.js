10adbe8318ca8ddf23b297fbd33f0ede01aeb23d
ading2210 <ading2019@gmail.com>
2024-02-09 20:10:00 +0000

    update url in unreleased scores tool

--- scores.js
+++ scores.js
@@ -9,15 +9,15 @@ function http_get(url, callback, headers=[], method="GET", content=null) {
 }
 
 function main() {
-  let url_regex = /https:\/\/app\.edulastic\.com\/home\/class\/([a-f0-9]+)\/test\/([a-f0-9]+)\/testActivityReport\/([a-f0-9]+)/;
+  let url_regex = /https:\/\/assessment\.peardeck\.com\/home\/class\/([a-f0-9]+)\/test\/([a-f0-9]+)\/testActivityReport\/([a-f0-9]+)/;
   if (!url_regex.test(window.location)) {
-    alert("Error: Invalid URL.\n\nFor reference, the URL should look like this:\nhttps://app.edulastic.com/home/class/CLASS_ID/test/TEST_ID/testActivityReport/TEST_REPORT_ID");
+    alert("Error: Invalid URL.\n\nFor reference, the URL should look like this:\nhttps://assessment.peardeck.com/home/class/CLASS_ID/test/TEST_ID/testActivityReport/TEST_REPORT_ID");
     return;
   }
   let matches = url_regex.exec(window.location);
   let group_id = matches[1];
   let test_id = matches[3];
-  let request_url = `https://app.edulastic.com/api/test-activity/${test_id}/report?groupId=${group_id}`;
+  let request_url = `https://assessment.peardeck.com/api/test-activity/${test_id}/report?groupId=${group_id}`;
   
   let token_list = JSON.parse(localStorage.getItem("tokens"));
   let token = localStorage.getItem(token_list[0]);
